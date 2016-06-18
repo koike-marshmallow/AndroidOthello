e1ebeea4c248620e2f13aa9dbe7d413c6ea1ba93
koike-marshmallow <taka.vao.321@gmail.com>
2016-06-18 11:56:55 +0900

    MoveStackを作成中

--- app/src/main/java/com/example/tkch/androidothello/core/MoveStack.java
+++ app/src/main/java/com/example/tkch/androidothello/core/MoveStack.java
@@ -1,5 +1,8 @@
 package com.example.tkch.androidothello.core;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by taka on 2016/06/18.
  */
@@ -14,16 +17,29 @@ public class MoveStack {
         public static final int FLP = 203;
         public static final int OTHERS = 291;
 
+        private int id;
+        private int gid;
+
         private int type;
         private XYPoint position;
         private int prevDisc;
         private int setDisc;
 
-        public MoveRecord(int tp0, XYPoint pt0, int pd0, int sd0){
+        public MoveRecord(int id0, int gi0, int tp0, XYPoint pt0, int pd0, int sd0){
+            id = id0;
+            gid = gi0;
             type = tp0;
             position = pt0;
             prevDisc = pd0;
             setDisc = sd0;
+        }
+
+        public int getId(){
+            return id;
+        }
+
+        public int getGroupId(){
+            return gid;
         }
 
         public int getType(){
@@ -43,8 +59,40 @@ public class MoveStack {
         }
 
         public String toString(){
-            return String.format("%s: [TYPE:%d, POS:(%d, %d), PREV:%d, SET:%d]",
-                    super.toString(), type, position.getX(), position.getY(), prevDisc, setDisc);
+            return String.format("%s: [ID:(%d(%d)), TYPE:%d, POS:(%d, %d), PREV:%d, SET:%d]",
+                    super.toString(), id, gid, type,
+                    position.getX(), position.getY(), prevDisc, setDisc);
         }
     }
+
+    public static final int NONE = OthelloDisc.NONE;
+    public static final int BLACK = OthelloDisc.BLACK;
+    public static final int WHITE = OthelloDisc.WHITE;
+
+    public static final int PUT = MoveRecord.PUT;
+    public static final int SET = MoveRecord.SET;
+    public static final int FLP = MoveRecord.FLP;
+    public static final int OTHERS = MoveRecord.OTHERS;
+
+    private List<MoveRecord> stack;
+    int gidCounter;
+
+    public MoveStack(){
+        init();
+    }
+
+    public void init(){
+        stack = new ArrayList<MoveRecord>();
+        gidCounter = 0;
+    }
+
+    public void push(int type, XYPoint pos, int prevDisc, int setDisc){
+        stack.add(new MoveRecord(stack.size(), gidCounter, type, pos, prevDisc, setDisc));
+    }
+
+    public void push(int type, int px, int py, int prevDisc, int setDisc){
+        push(type, new XYPoint(px, py), prevDisc, setDisc);
+    }
+
+
 }
